4baca1f25ed0ba33f346ae042612ecf3fd6ffa63
Min Kim <mk4555@nyu.edu>
2017-11-02 23:11:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,10 @@ class Anagram
   end
 
   def match(words)
-    tmp = @word.split("").sort! #split self into letters ["a","d","e","i","p","r","s"]
+    tmp = @word.split("").sort!.join #split self into letters ["a","d","e","i","p","r","s"]
     tmp = tmp.join
     binding.pry
-    words.map!{|word| word.split("")} #split
+    words.map{|word| word.split("")} #split
   end
 
 end
