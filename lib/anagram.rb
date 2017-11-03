f96c3b801ff2f904227541fc65e2a2dfea8398bb
Min Kim <mk4555@nyu.edu>
2017-11-03 02:04:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def match(words)
     tmp = @word.split("").sort!.join #split self into letters and join
     temp = words.map{|word| word if word.split("").sort.join == tmp} #split
-    temp
+    temp.compact
 
   end
 
