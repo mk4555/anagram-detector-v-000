6bee998d12c6aa9174f19099c0a6e82cd2fb5c06
Min Kim <mk4555@nyu.edu>
2017-11-03 02:04:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require('pry')
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+    @word
+  end
+
+  def match(words)
+    tmp = @word.split("").sort!.join #split self into letters and join
+    temp = words.map{|word| word if word.split("").sort.join == tmp} #split
+    temp.compact
+
+  end
+
+end
